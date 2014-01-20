edc5a33bddf0be5ef86f61e0f9383862effa3e0d
sualko <github@spam.herberth.eu>
2014-01-20 13:59:07 +0100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/* global module:false */
+module.exports = function(grunt) {
+
+   // Project configuration.
+   grunt.initConfig({
+      app: grunt.file.readJSON('app.json'),
+      meta: {
+         banner: grunt.file.read('js/jsxc/banner.js')
+      },
+      jshint: {
+         options: {
+            jshintrc: '.jshintrc'
+         },
+         gruntfile: {
+            src: 'Gruntfile.js'
+         },
+         files: [ 'js/ojsxc.js' ]
+      },
+      copy: {
+         main: {
+            files: [ {
+               expand: true,
+               src: [ 'js/*.js', 'css/*', 'ajax/*', 'img/*', 'LICENSE' ],
+               dest: 'build/'
+            }, {
+               expand: true,
+               cwd: 'js/jsxc/build/',
+               src: [ '**' ],
+               dest: 'build/js/jsxc/'
+            } ]
+         }
+      },
+      clean: [ 'build/' ],
+      usebanner: {
+         dist: {
+            options: {
+               position: 'top',
+               banner: '<%= meta.banner %>'
+            },
+            files: {
+               src: [ 'build/js/*.js' ]
+            }
+         }
+      }
+   });
+
+   // These plugins provide necessary tasks.
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-banner');
+
+   // Default task.
+   grunt.registerTask('default', [ 'jshint', 'clean', 'copy', 'usebanner' ]);
+
+};
---
+++ js/sjsxc.js
@@ -0,0 +1,142 @@
+(function($, pt){
+
+if(typeof configureLinksInMessage === "function"){
+	var configureLinksInMessageOld = configureLinksInMessage;
+
+	configureLinksInMessage = function(){ 
+		configureLinksInMessageOld();		
+
+		var spot = $("<span>X</span>").addClass("jsxc_spot");
+		var mails = $('#messageContent').find('a[href^="mailto:"]');
+		mails.each(function(){
+			var href = $(this).attr("href").replace(/^ *mailto:/, "").trim();
+			if(href != Strophe.getBareJidFromJid(jsxc.storage.getItem("jid"))){
+				var cid = jsxc.jidToCid(href);
+				var s = spot.clone().addClass("jsxc_buddy_"+cid);
+				$(this).before(s);
+	console.log(jsxc.storage.getUserItem('buddy_' + cid));
+				if(jsxc.storage.getUserItem('buddy_' + cid)){
+					jsxc.gui.update(cid); console.log("Update buddy");
+				} else {
+					console.log("not found "+cid);
+				}
+			}
+		});		
+		console.log("Add X to " + mails.length);
+		
+	}
+}
+
+function onRosterToggle(event, state, duration){
+    var wrapper = $('#rightPanel');
+    var control = $('#toolbar');
+    
+    var roster_width = (state == 'shown')? $('#jsxc_roster').outerWidth() : 0;
+    //var navigation_width = $('#navigation').width();
+       
+    wrapper.animate({marginRight: (roster_width) + 'px'}, duration);
+    control.animate({marginRight: (roster_width) + 'px'}, duration, 'swing', function(){
+if(typeof SOGoResizableTableInterface != 'undefined')
+SOGoResizableTableInterface.resize.call(pt('messageListHeader'));	
+});
+}
+
+function onRosterReady() {
+console.log('onRosterReady');
+    var roster_width = $('#jsxc_roster').outerWidth();
+    //var navigation_width = $('#navigation').width();
+    var roster_right = parseFloat($('#jsxc_roster').css('right'));
+    var mr = (200 + ($.isNumeric(roster_right)?roster_right:0));
+console.log(mr);   
+console.log(roster_width); 
+console.log($.isNumeric(roster_right)?roster_right:0);
+$('#toolbar').css('marginRight', mr + 'px');
+    $('#rightPanel').css('marginRight', mr + 'px');
+if(typeof SOGoResizableTableInterface != 'undefined')
+    SOGoResizableTableInterface.resize.call(pt('messageListHeader'));
+}
+
+//initialization
+$(function() {
+
+   if($('#linkBanner').length == 0){
+      return 0;
+   }
+   
+	$("head").append($("<link rel='stylesheet' href='/SOGo.woa/WebServerResources/jsxc/css/jquery.colorbox.css' type='text/css' media='screen' />"));
+
+	$("head").append($("<link rel='stylesheet' href='/SOGo.woa/WebServerResources/jsxc/css/main.css' type='text/css' media='screen' />"));
+	
+
+    $(document).on('ready.roster.jsxc', onRosterReady);
+    $(document).on('toggle.roster.jsxc', onRosterToggle);
+
+    if(jsxc.storage.getItem("abort")) return;
+
+    jsxc.init({
+        loginForm: {
+            form: '#connectForm',
+            jid: '#userName',
+            pass: '#password',
+            preJid: function(jid) {
+
+                var resource = '';
+                var domain = 'localhost';
+                var boshUrl = '/bosh/';
+                
+                jsxc.storage.setItem('boshUrl', boshUrl);
+                
+                if (jid.match(/@(.*)$/))
+                    return (jid.match(/\/(.*)$/)) ? jid : jid + resource;
+                else
+                    return jid + '@' + domain + resource;
+            }
+        },
+        logoutElement: $('#logoff'),
+        checkFlash: false,
+        debug: function(msg, data) {
+            if(data)
+                console.log(msg, data);
+            else
+                console.log(msg);
+        },
+        rosterAppend: 'body',
+        root: '/SOGo.woa/WebServerResources/jsxc/',
+        //@TODO: don't include get turn credentials routine into jsxc
+        turnCredentialsPath: null,
+        formFound: function(){
+            var submit = pt("submit");
+                submit.stopObserving("click", onLoginClick);
+                var userName = pt("userName");
+                userName.stopObserving("keydown", onFieldKeyDown);
+                var passw = pt("password");
+                passw.stopObserving("keydown", onFieldKeyDown); 
+                
+            $('#connectForm').submit(onLoginClick);
+            $('#submit').click(function(){
+	console.log("submit click");
+                $('#connectForm').submit();
+                });
+            $('#userName, #password').keypress(function(ev){
+                if(ev.which !== 13)
+                    return;
+                    
+                $('#connectForm').submit();
+            });
+        }
+    });
+
+
+    //Add submit link without chat functionality
+    if (jsxc.el_exists($('#body-login form'))) {
+
+        var link = $('<a/>').text('Log in without chat').click(function() {
+            jsxc.submitLoginForm();
+        });
+        
+        var alt = $('<p id="jsxc_alt"/>').append(link);
+        $('#body-login form fieldset').append(alt);
+    }
+}); 
+
+})(jQuery, $);
